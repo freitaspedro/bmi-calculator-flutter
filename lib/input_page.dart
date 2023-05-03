5793c6127afa2d4fb036ab540b92ea0094e22a31
freitaspedro <pfreitas@id.uff.br>
2023-05-03 14:40:40 -0300

    pass ontap func as param

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -34,37 +34,33 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: GestureDetector(
+                    child: ReusableCard(
+                      color: (selectedGender == Gender.male)
+                          ? activeCardColor : inactiveCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
                       onTap: () {
                         setState(() {
                           selectedGender = Gender.male;
                         });
                       },
-                      child: ReusableCard(
-                        color: (selectedGender == Gender.male)
-                            ? activeCardColor : inactiveCardColor,
-                        cardChild: IconContent(
-                          icon: FontAwesomeIcons.mars,
-                          label: 'MALE',
-                        ),
-                      ),
                     )
                   ),
                   Expanded(
-                    child: GestureDetector(
+                    child: ReusableCard(
+                      color: (selectedGender == Gender.female)
+                          ? activeCardColor : inactiveCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
                       onTap: () {
                         setState(() {
                           selectedGender = Gender.female;
                         });
                       },
-                      child: ReusableCard(
-                        color: (selectedGender == Gender.female)
-                            ? activeCardColor : inactiveCardColor,
-                        cardChild: IconContent(
-                          icon: FontAwesomeIcons.venus,
-                          label: 'FEMALE',
-                        ),
-                      ),
                     )
                   ),
                 ],
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -4,17 +4,21 @@ class ReusableCard extends StatelessWidget {
 
   final Color color;
   final Widget cardChild;
-  ReusableCard({@required this.color, this.cardChild});
+  final Function onTap;
+  ReusableCard({@required this.color, this.cardChild, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 
